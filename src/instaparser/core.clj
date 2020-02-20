c9c27b112a9ce83cbdde2fe1788579a3638ce98d
Chandan Kumar <cksharma122@gmail.com>
2020-02-20 11:30:28 +0530

    Implementing function defination

--- src/instaparser/core.clj
+++ src/instaparser/core.clj
@@ -1,6 +1,8 @@
 (ns instaparser.core
  (:require [instaparse.core :as insta]))
 
+(def global-object (atom {}))
+
 (def transform-options
  {:number read-string
 	:add    (partial +)
@@ -15,11 +17,21 @@
 	:div '/
 	:mul '*))
 
+
+(defmacro define-fn [args body]
+ `(fn ~args ~body))
+
+(defn declare-function [fn-name args body]
+ (swap! global-object assoc fn-name (define-fn args body)))
+
 (defn transform-tree [tree]
+ (println tree)
  (let [node (first tree) s (second tree) terms (rest tree) length-expr (count terms)]
 	(case node
 	 :number (read-string s)
 	 :operator (choose-operator s)
+	 :args (mapv second terms)
+	 :function (declare-function (second s) (transform-tree (second terms)) (transform-tree (last terms)))
 	 :expr (if (= length-expr 1)
 					(transform-tree (first terms))
 					(list (transform-tree (second terms))
@@ -28,7 +40,11 @@
 
 (def parser
  (insta/parser
-	"expr =  number | <'('> number space operator space number space <')'> | <'('> expr space operator space expr <')'>
+	"language = function | expr
+	expr =  number | <'('> number space operator space number space <')'> | <'('> expr space operator space expr <')'>
+	function = <'fun'> space+ identifier space+ args space+ <':'> space expr
+	identifier = #'[a-z]+[0-9]*[a-z,A-Z]*'
+	args = <'['> (identifier space+)* identifier <']'>
 	operator = add | sub | div | mul
 	add = <'+'>
 	sub = <'-'>
@@ -38,4 +54,4 @@
 	<space> = <#' '*>"))
 
 (defn parse [input]
- (->> (parser input) transform-tree))
+ (->> (parser input) second transform-tree))
