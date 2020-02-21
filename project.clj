21c5871eae5d804ef08942f7cdf6c681687c2202
Shubham Chauhan <shubham25may1998@gmail.com>
2020-02-21 12:26:53 +0530

    Deepika | Chandan | Shubham : Introduce recursion without end condition

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
 								 [instaparse "1.4.10"]]
-  :repl-options {:init-ns instaparser.core})
+  :repl-options {:init-ns instaparser.core}
+  :main instaparser.core)
--- src/instaparser/core.clj
+++ src/instaparser/core.clj
@@ -1,14 +1,12 @@
 (ns instaparser.core
   (:require [instaparse.core :refer :all]))
 
-;(def global-object (atom {}))
-
 (defn choose-operator [operator]
   (case (first operator)
-    :add '+
-    :sub '-
-    :div '/
-    :mul '*))
+    :add 'clojure.core/+
+    :sub 'clojure.core/-
+    :div 'clojure.core//
+    :mul 'clojure.core/*))
 
 (def user-namespace (create-ns 'fun-lang.user))
 
@@ -18,9 +16,13 @@
     (in-ns 'instaparser.core)
     value))
 
+
 (defn declare-function [fn-name args body]
-  (save fn-name
-        (eval (map read-string (list "fn" args body)))))
+  (let [fn-elements (map read-string (list "clojure.core/defn" fn-name args body))]
+    (do
+      (in-ns 'fun-lang.user)
+      (eval fn-elements)
+      (in-ns 'instaparser.core))))
 
 (defn switch-ns-and-eval [fn-call]
   (do (in-ns 'fun-lang.user)
@@ -37,11 +39,11 @@
       :number (read-string s)
       :identifier (symbol s)
       :operator (choose-operator s)
-      :fn-call (switch-ns-and-eval (conj (map transform-tree (rest terms)) (transform-tree s)))
+      :fn-call (conj (map transform-tree (rest terms)) (transform-tree s))
       :args (str (mapv (comp symbol second) terms))
       :val (save (transform-tree s) (transform-tree (last tree)))
       :function (declare-function
-                  (transform-tree s)
+                  (second s)
                   (transform-tree (second terms))
                   (str (transform-tree (last terms))))
       :expr (if (= length-expr 1)
@@ -57,9 +59,13 @@
   (->> input
        grammer-rules
        second
-       transform-tree))
+       transform-tree
+       switch-ns-and-eval))
 
 (defn -main []
+  (println "Welcome to Fun-Lang!")
   (loop []
+    (print (str (ns-name user-namespace) ":=> "))
+    (flush)
     (println (parse-my-lang (read-line)))
     (recur)))
